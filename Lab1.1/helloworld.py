aa9b891f20a2aafa1b46c7e74614697d9b52ff41
svmungalov <sv.mungalov@jet.msk.su>
2017-06-08 18:41:00 +0300

    Lab 4

--- Lab1.1/helloworld.py
+++ Lab1.1/helloworld.py
@@ -1,1 +1,39 @@
 print ("Hello World!")
+
+class Subnet:
+    def __init__(self, n="0.0.0.0", p="/0"):
+        self.network = n
+        self.prefix = p
+    def __str__(self):
+        return self.network + self.prefix
+    def getnet(self):
+     return self.network
+    def getprefix(self):
+        return self.prefix
+
+n1 = Subnet("192.168.1.0", "/24")
+n2 = Subnet("172.16.0.0", "/16")
+n3 = Subnet()
+x1 = n1.getnet()
+x2 = n2.getprefix()
+print (n1)
+print (n2)
+print (n3)
+print (x1)
+print (x2)
+
+class Addr_plan_entry(Subnet):
+    def __init__(self, n="0.0.0.0", p="/0", gw="0.0.0.0"):
+        Subnet.__init__(self, n, p)
+        self.gateway = gw
+    def getgw(self):
+        return self.gateway
+    def __str__(self):
+        return "Network: %s, prefix %s, gateway: %s" \
+        %(self.network, self.prefix, self.gateway)
+
+n4 = Addr_plan_entry("10.1.1.0", "/24", "10.1.1.1")
+print (n4)
+n5 = Addr_plan_entry("10.10.10.0", "/24", "10.10.10.1")
+print (n5.getnet())
+print (n5.getgw())
---
+++ Lab1.4/ip_networks.py
@@ -0,0 +1,23 @@
+from ipaddress import IPv4Network
+import random
+
+class IPv4RandomNetwork(IPv4Network):
+    def __init__(self):
+        IPv4Network.__init__(self, (random.randint(0x0B000000, 0xDF000000),
+        random.randint(8,24)), strict=False)
+    def value(self):
+        return int(self.network_address) + int(self.netmask) * 2**32
+
+def sortfunc(x):
+    return x.value()
+
+i=1
+l =[]
+while (i < 100):
+    l.append (IPv4RandomNetwork())
+    i = i + 1
+print (l)
+
+l1 = sorted(l, key=sortfunc)
+for i in l1:
+    print(i)
